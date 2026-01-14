727a608f83e7219bb06a67bcf07e82ad649826cf
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-01-14 09:52:57 -0600

    updated datasource type

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,9 +1,8 @@
-import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./user/user.entity";
 
 export const AppDataSource = new DataSource({
-    type: "sqlite", // or "mysql", "sqlite", etc.
+    type: "better-sqlite3", // or "mysql", "sqlite", etc.
     database: process.env.DB_DATABASE || "userDB.sqlite",
     synchronize: true, // Use carefully in production
     logging: false,
